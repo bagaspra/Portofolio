fcd7b0ad7304db7f94e9f0bc61c5244491adf631
bagaspra <bagasprasetyo36@gmail.com>
2022-10-16 00:48:19 +0800

    new porto

--- next.config.js
+++ next.config.js
@@ -2,12 +2,6 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
-  images: {
-    loader: 'akamai',
-    path: '',
-  },
-  basePath: '/Portofolio',
-  assetPrefix: '/Portofolio',
-};
+}
 
-module.exports = nextConfig;
+module.exports = nextConfig
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,14 +1,15 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  darkMode: 'class',
+  darkMode: "class",
   content: [
-    './pages/**/*.{js,ts,jsx,tsx}',
-    './components/**/*.{js,ts,jsx,tsx}',
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
     extend: {
       fontFamily: {
-        burtons: 'burtons',
+        burtons: "burtons",
+        poppins: ["Poppins", "sans-serif"],
       },
     },
   },
